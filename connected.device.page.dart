7dd3abe3d42daacce12ff1f7b604a8dc379e9cc1
Dripperx <98571407+ROUNGKAW12@users.noreply.github.com>
2023-07-24 08:41:29 +0700

    Update connected.device.page.dart

--- connected.device.page.dart
+++ connected.device.page.dart
@@ -1,22 +1,22 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:logging/logging.dart';
 
 class OximeterBluetoothCharacteristic {
   static const oxygen = "cdeacb81-5235-4c07-8846-93a37ee6b86d";
 }
 
-class OximeterBluetootService {
+class OximeterBluetoothService {
   static const oxygen = "cdeacb80-5235-4c07-8846-93a37ee6b86d";
 }
 
 class ConnectDevicePage extends StatefulWidget {
   const ConnectDevicePage({
-    super.key,
+    Key? key,
     required this.title,
     required this.device,
-  });
+  }) : super(key: key);
 
   final String title;
   final BluetoothDevice device;
@@ -27,12 +27,14 @@ class ConnectDevicePage extends StatefulWidget {
 
 class _ConnectDevicePageState extends State<ConnectDevicePage> {
   List<BluetoothService> services = [];
-  StreamSubscription? sub;
+  StreamSubscription<List<int>>? sub;
   late BluetoothCharacteristic targetChar;
 
   int x = -1;
   int y = -1;
   int z = -1;
+
+  final Logger logger = Logger('ConnectDevicePage');
 
   @override
   void initState() {
@@ -42,11 +44,10 @@ class _ConnectDevicePageState extends State<ConnectDevicePage> {
 
   void findTargetChar() async {
     List<BluetoothService> services = await widget.device.discoverServices();
-    BluetoothService targetService = services.firstWhere(
-        (service) => service.uuid.toString() == OximeterBluetootService.oxygen);
-    BluetoothCharacteristic targetChar = targetService.characteristics
-        .firstWhere((char) =>
-            char.uuid.toString() == OximeterBluetoothCharacteristic.oxygen);
+    BluetoothService targetService = services.firstWhere((service) =>
+        service.uuid.toString() == OximeterBluetoothService.oxygen);
+    targetChar = targetService.characteristics.firstWhere((char) =>
+        char.uuid.toString() == OximeterBluetoothCharacteristic.oxygen);
     sub = targetChar.value.listen((event) {
       if (event[0] == 129) {
         x = event[1];
@@ -55,72 +56,87 @@ class _ConnectDevicePageState extends State<ConnectDevicePage> {
         setState(() {});
       }
 
-      print(event);
+      logger.info(event);
     });
     targetChar.setNotifyValue(!targetChar.isNotifying);
     setState(() {});
   }
 
+  void resetValues() {
+    setState(() {
+      x = 0;
+      y = 0;
+      z = 0;
+    });
+  }
+
   @override
   void dispose() {
     super.dispose();
-    if (sub != null) sub!.cancel();
+    sub?.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        body: Container(
-          child: Column(
-            children: [
-              Text(
-                "$x $y $z",
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 60),
+            Text.rich(
+              TextSpan(
+                style: const TextStyle(
+                    fontFamily: 'Nunito',
+                    fontSize: 24,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.deepPurple),
+                text: 'Value : \n',
+                children: [
+                  TextSpan(
+                    text: '$x bpmPR  \n',
+                    style: const TextStyle(
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromARGB(255, 246, 144, 11),
+                    ),
+                  ),
+                  TextSpan(
+                    text: '$y %SpO2 \n',
+                    style: const TextStyle(
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromARGB(255, 168, 216, 21),
+                    ),
+                  ),
+                  TextSpan(
+                    text: '$z PI% \n',
+                    style: const TextStyle(
+                      fontFamily: 'Nunito',
+                      fontWeight: FontWeight.w700,
+                      color: Color.fromARGB(255, 246, 11, 109),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ));
-  }
-
-  List<Widget> servicesList() {
-    List<Widget> list = services.map((service) {
-      List<BluetoothCharacteristic> charsList = service.characteristics;
-
-      List<Widget> charsWidget = charsList.map(
-        (char) {
-          if (OximeterBluetoothCharacteristic.oxygen != char.uuid.toString()) {
-            return const SizedBox.shrink();
-          }
-          return Column(
-            children: [
-              Text("char id: ${char.uuid.toString()}"),
-              Text("$x $y $z"),
-              ElevatedButton(
-                  onPressed: () {
-                    char.setNotifyValue(!char.isNotifying);
-                    setState(() {});
-                  },
-                  child: Text("${char.isNotifying}"))
-            ],
-          );
-        },
-      ).toList();
-      if (OximeterBluetootService.oxygen != service.uuid.toString()) {
-        return const SizedBox.shrink();
-      }
-      return Column(
-        children: [
-          Text(
-            service.uuid.toString(),
-          ),
-          Column(
-            children: charsWidget,
-          )
-        ],
-      );
-    }).toList();
-    return list;
+            ),
+            ElevatedButton(
+              onPressed: resetValues,
+              child: Text('Reset Values'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
